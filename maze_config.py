5078201806c08ad4cdddd6d62c5c42d44b012762
Me <arobasedan@gmail.com>
2026-04-10 23:26:41 +0200

    feat: fait mon propre config parser et config validation (juste pour comprendre de mon cote)

---
+++ maze_config.py
@@ -0,0 +1,27 @@
+from typing import Tuple, Self
+from pydantic import BaseModel, Field, model_validator
+
+
+class MazeConfig(BaseModel):
+    width: int = Field(gt=0)
+    height: int = Field(gt=0)
+    entry: Tuple[int, int]
+    exit: Tuple[int, int]
+    output_file: str = Field(min_length=1)
+    perfect: bool
+    seed: int | None = None
+
+    def bounds_check(self, position: Tuple[int, int]) -> bool:
+        x, y = position
+        return 0 < x < self.width and 0 < y < self.height
+
+    @model_validator(mode="after")
+    def entry_and_exit_validation(self) -> Self:
+        if self.entry == self.exit:
+            raise ValueError("Entry can't be the same as exit")
+        if (
+            not self.bounds_check(self.entry)
+            or not self.bounds_check(self.exit)
+        ):
+            raise ValueError("Entry and exit must be within the maze bounds")
+        return self
